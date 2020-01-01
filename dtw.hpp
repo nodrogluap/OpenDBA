03de24d9dce425fe1f51cd38caf6907cc2be5816
Paul Gordon <gordonp@ucalgary.ca>
2020-01-01 15:42:39 -0700

    Added comp shortcut if open_end used and 1st seq much shorter than 2nd

--- dtw.hpp
+++ dtw.hpp
@@ -60,6 +60,59 @@ __global__ void DTWDistance(const T *first_seq_input, const size_t first_seq_inp
 	// Each thread will be using the same second sequence value throughout the rest of the kernel, so store it as a local variable for efficiency.
 	const T second_seq_thread_val = offset_within_second_seq+threadIdx.x >= second_seq_length ? 0 : second_seq[offset_within_second_seq+threadIdx.x];
 
+	// Possible shortcut: If we are allowing open right moves only at the end of the alignment, 
+	// and the top row of the matrix is in that state, and it's the lowest cost option in this column
+	// of the DTW cost matrix, there is no need to continue computing further grid X elements. Why? 
+	// Because any other path will just get more expensive, while the open right move can continue
+	// eating up the subject to get to the upper right corner of the DTW matrix without any additional cost.
+	// This will speed up prefix searches in particular, where the prefix (1st seq) length is a small proportion of the 2nd's.
+	// We don't even need to be keeping the pathMatrix to know we are in that state, because it's the overriding move choice (see used_open_right_end_cost below)
+	// when you're at the top of the marix in open end mode.
+	if(offset_within_second_seq > first_seq_length && use_open_end && !use_open_start){
+		// Check if the search has already been abrogated by a previous kernel call (further left in the DTW matrix calculation) 
+		if(dtwCostSoFar[0] == numeric_limits<T>::max()){
+			if(pathMatrix != 0 && offset_within_second_seq+threadIdx.x < second_seq_length){
+                                pathMatrix[pitchedCoord(offset_within_second_seq+threadIdx.x,first_seq_length-1,pathMemPitch)] = OPEN_RIGHT;
+                        }
+			return;
+		}
+		
+		// Otherwise map/reduce within this kernel to pretty efficiently find the minimum value across the 1D dtwCostSoFar array without variable length threadblock shared memory.
+		T minval = threadIdx.x < first_seq_length ? dtwCostSoFar[threadIdx.x] : numeric_limits<T>::max(); 
+		for(int i = 1; i*blockDim.x < first_seq_length; i++){ 
+			// Assign each thread to find the minimum values strided (by # threads doing work) across the length of the first sequence. 
+			if(i*blockDim.x+threadIdx.x < first_seq_length && minval > dtwCostSoFar[i*blockDim.x+threadIdx.x]){
+				// Hopefully mostly coalesced memory access
+				minval = dtwCostSoFar[i*blockDim.x+threadIdx.x];
+			}
+		}
+		minval = warpReduceMin<T>(minval); // across the warp
+		int lane = threadIdx.x % CUDA_WARP_WIDTH;
+    		int wid = threadIdx.x / CUDA_WARP_WIDTH;
+		T *warp_minvals = costs; // threadblock shared memory space is the same as the costs pseudo 2D array since we won't need it outside of this block 
+		if(!lane) warp_minvals[wid] = minval;
+      		__syncthreads();  
+      		// Get in-bounds values only for final threadblock reduction, calculated by the first warp's threads (threadblock may not be full).
+      		if(!wid){
+			minval = (threadIdx.x < blockDim.x / CUDA_WARP_WIDTH) ? warp_minvals[lane] : numeric_limits<T>::max();
+			warp_minvals[0] = warpReduceMin<T>(minval); // across all threads in the block
+		}
+      		__syncthreads();  
+
+		// Top row value is the lowest for this column, only need to populate the open_right move for correct backtracking and cumulative cost calcs
+		if(dtwCostSoFar[first_seq_length-1] == warp_minvals[0]){
+			if(pathMatrix != 0 && offset_within_second_seq+threadIdx.x < second_seq_length){
+				pathMatrix[pitchedCoord(offset_within_second_seq+threadIdx.x,first_seq_length-1,pathMemPitch)] = OPEN_RIGHT;
+			}
+			// Make sure bottom row's DTW cost so far is set to the max possible. 
+			// This is how we indicate that we've decided to abrogated the rest of the search.
+			if(threadIdx.x == 0){
+				dtwCostSoFar[0] = numeric_limits<T>::max();
+			}
+			return;
+	  	}
+	}
+
 	if(threadIdx.x == 0){
 		// Populate the bottom row of the vertical swath on every kernel invocation, this can't be done in parallel.
 		const T first_seq_start_val = first_seq[0];
@@ -162,7 +215,7 @@ __global__ void DTWDistance(const T *first_seq_input, const size_t first_seq_inp
 	}
 
 	// If this is the end of the second sequence, we now know the total cost of the alignment and can populate 
-	// global var dtwPairwiseDistances.
+	// global var dtwPairwiseDistances. This is more efficient than doing a round trip on the PCI bus to the CPU for the same purpose.
 	if(offset_within_second_seq+blockDim.x >= second_seq_length){
 		if(dtwPairwiseDistances != 0 && threadIdx.x == 0){
 			// 1D index for row into distances upper left pairs triangle is the total size of the triangle, minus all those that haven't been processed yet. 
