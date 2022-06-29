73645c98c90a840826910d5ab396059819bfd813
Paul Gordon <gordonp@ucalgary.ca>
2022-06-29 15:03:58 -0600

    Create rna_multimodality.sh

---
+++ rna_multimodality.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [[ $# -ne 2 ]]; then
+    echo "Usage: $0 <length of reference RNA sequence> <OpenDBA cluster file prefix>" >&2
+    exit 2
+fi
+
+trap 'rm -f "$TMPFILE"' EXIT
+
+TMPFILE=$(mktemp) || exit 1
+perl -ane 'push @{$means{$F[2]}}, $F[1] unless $F[4] eq "OPEN_RIGHT" or $#F < 4;END{for (sort {$b <=> $a} keys %means){print '$1'-$_,"\t", join(",", @{$means{$_}}),"\n"}}' $2.path*.txt > $TMPFILE
+`dirname $0`/diptest.R $TMPFILE > $2.multimodal.diptest.txt
