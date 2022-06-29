f46f9f7b4f68765e4f20e7d9548b8587970f1543
Paul Gordon <gordonp@ucalgary.ca>
2022-06-29 14:48:01 -0600

    Initial version of Hartigans' Dip Test of multimodality

---
+++ diptest.R
@@ -0,0 +1,24 @@
+#!/usr/bin/env Rscript
+library(diptest)
+
+args = commandArgs(trailingOnly=TRUE)
+if (length(args)!=1) {
+  stop("Usage: diptest.R <opendba-means output.txt>\n", call.=FALSE)
+} 
+
+file <- args[1]
+con <- file(description=file, open="r")
+ 
+com <- paste("wc -l ", file, " | awk '{ print $1 }'", sep="")
+n <- system(command=com, intern=TRUE)
+ 
+pvals <- c()
+poss <- c()
+for(i in 1:n) {
+  tmp <- readLines(con=con, n=1); 
+  vals <- unlist(strsplit(tmp, '\t')); 
+  poss <- append(poss, vals[1]); 
+  pvals <- append(pvals, dip.test(as.numeric(unlist(strsplit(as.character(vals[2]),','))))$p.value)
+}
+qvals <- p.adjust(pvals, method="fdr")
+which(qvals < .05) + as.numeric(poss[1])
