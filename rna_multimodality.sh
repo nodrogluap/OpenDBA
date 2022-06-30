25172e8c2ebbd3ae562c3a5239311b23e789ea5d
Paul Gordon <gordonp@ucalgary.ca>
2022-06-30 09:18:30 -0600

    Added KDE + Excess Mass Tests

--- rna_multimodality.sh
+++ rna_multimodality.sh
@@ -9,3 +9,4 @@ trap 'rm -f "$TMPFILE"' EXIT
 TMPFILE=$(mktemp) || exit 1
 perl -ane 'push @{$means{$F[2]}}, $F[1] unless $F[4] eq "OPEN_RIGHT" or $#F < 4;END{for (sort {$b <=> $a} keys %means){print '$1'-$_,"\t", join(",", @{$means{$_}}),"\n"}}' $2.path*.txt > $TMPFILE
 `dirname $0`/diptest.R $TMPFILE > $2.multimodal.diptest.txt
+`dirname $0`/kde_smoothing_plus_excess_mass.R $TMPFILE > $2.multimodal.kde_smoothing_plus_excess_mass.txt
